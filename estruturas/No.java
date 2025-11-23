d829c029c328c95ea8480f7e9d428f857667c3cb
Kaiobolsoni <kaiobolsonioriginal@gmail.com>
2025-11-23 18:04:43 -0300

    refactor: no

--- estruturas/No.java
+++ estruturas/No.java
@@ -1,10 +1,6 @@
-<<<<<<<< HEAD:estruturas/No.java
 package estruturas;
-
-========
-package src.estruturas;
->>>>>>>> 0817e2ac622a81dcb192e702100beca20a8bf967:src/Estruturas/No.java
 public class No {
+
     int valor;
     No esquerda, direita;
     int altura;
